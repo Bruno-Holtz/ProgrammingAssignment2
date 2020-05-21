2b665549d97fbf6b36a2bd16bcb4bf15902c73d4
Bruno-Holtz <64865866+Bruno-Holtz@users.noreply.github.com>
2020-05-20 23:53:25 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  if(is.matrix(x) && nrow(x) == ncol(x)){
+    set <- function(y){
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(Y) inv <<- Y
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+  } else {
+    message('input must be a square matrix')
+    return()
+  }
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  y <- x$getinv()
+  if(!is.null(y)){
+    message('Getting cache data')
+    return(y)
+  }
+  pivot <- x$get()
+  y <- solve(pivot, ...)
+  x$setinv(y)
+  y
 }
